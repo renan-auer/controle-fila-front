c1e8c8587ce167c14c185c051f41291149b5c967
renan-auer <renan.auer@gmail.com>
2021-05-20 02:19:32 -0300

    feat: set routes and modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientDashboardComponent } from './client-dashboard/client-dashboard.component';
 import { DefinirPerfilComponent } from './definir-perfil/definir-perfil.component';
+import { GerenteDashboardComponent } from './gerente-dashboard/gerente-dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'definir-perfil', pathMatch: 'full' },
   { path: 'definir-perfil', component: DefinirPerfilComponent },
+  { path: 'app/gerente', component: GerenteDashboardComponent },
+  { path: 'app/cliente', component: ClientDashboardComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { DefinirPerfilComponent } from './definir-perfil/definir-perfil.component';
+import { ClientDashboardComponent } from './client-dashboard/client-dashboard.component';
+import { GerenteDashboardComponent } from './gerente-dashboard/gerente-dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SenhaAtualComponent } from './senha-atual/senha-atual.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DefinirPerfilComponent
+    DefinirPerfilComponent,
+    ClientDashboardComponent,
+    GerenteDashboardComponent,
+    SenhaAtualComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
